20fe92759cb447566cacd06b1c1db811f72af496
PabloPicoTres <SoyPabloJajaSalu2@gmail.com>
2020-03-28 17:54:32 +0100

    clases de eventos y locales creadas, necesitan perfilar
    Signed-off-by: PabloPicoTres <SoyPabloJajaSalu2@gmail.com>

--- app/src/main/java/es/fiestasgranada/main/HomeActivity.java
+++ app/src/main/java/es/fiestasgranada/main/HomeActivity.java
@@ -6,24 +6,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Toast;
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class HomeActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
-
-
-    //defino listadoEventos y Listado a nivel de clase para poder acceder a ellos desde el metodo de abajo, para los click vaya
-
-    private ListView listadoEventos;
-    private List<String> listado;
+public class HomeActivity extends AppCompatActivity {
 
     //-------------------------------------------------------------
 
@@ -31,6 +16,10 @@ public class HomeActivity extends AppCompatActivity implements AdapterView.OnIte
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //inicia el fragment
+
+
 
 
         //Initialize and Assign Variable
@@ -61,37 +50,6 @@ public class HomeActivity extends AppCompatActivity implements AdapterView.OnIte
             }
         });
 
-        //Lista de eventos en el home
-        listadoEventos = (ListView) findViewById(R.id.ListadoEventos);
-
-        //He hecho un array de string, pero haremos un objeto que sea evento,
-        //con hijos que sean Fiesta, Promocion, ETC y el array sera de eventos
-        listado = new ArrayList<>();
-        listado.add("FIESTA PRIMAVERA EN BABI");
-        listado.add("JUERNES EN MAE");
-        listado.add("ADRI ES GEI");
-
-        //un adaptador para unir la lista de eventos y el array
-        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(
-                this,
-                android.R.layout.simple_list_item_1,
-                listado
-        );
-
-        listadoEventos.setAdapter(adaptador);
-
-        //evento click con cada elemento de la lista
-
-        listadoEventos.setOnItemClickListener(this);
-
-
     }
 
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        //Esto es lo que se ejecuta cuando el elemento de la lista es clickeado
-        Toast.makeText(this, "Adri es gei", Toast.LENGTH_SHORT).show();
-
-        //mas tarde haremos distinciones entre los objetos que hemos clickeado
-    }
 }
